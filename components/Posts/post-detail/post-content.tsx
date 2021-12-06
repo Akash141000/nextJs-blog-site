f970103d286512d8d6e917ef70e17fb475110749
akash padwal <akashpadwal21@gmail.com>
2021-12-06 13:45:28 +0530

    post content fixed

--- components/Posts/post-detail/post-content.tsx
+++ components/Posts/post-detail/post-content.tsx
@@ -1,25 +1,15 @@
 import PostHeader from "./post-header";
 import ReactMarkdown, { Components } from "react-markdown";
 import { IPost } from "../../../lib/posts-util";
-import Image from "next/dist/client/image";
-
-const DUMMY_POST = {
-  slug: "getting-started with nextJs3",
-  title: "Nextjs",
-  image: "nextjs-cover.jpg",
-  date: "2022-02-10",
-  content: "# This is a first post",
-};
 
 const PostContent: React.FC<{ post: IPost }> = (props) => {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  const imagePath = `/images/posts/${props.post.slug}/${props.post.image}`;
 
   const renderers: Components = {
-    ul: (node, ...props) => {
+    ul: ({node, ...props}) => {
       return (
-        <ul className="list-disc m-5" {...props}>
-          {node.children}
-        </ul>
+        <ul className="list-disc m-5" {...props}>          
+          </ul>
       );
     },
   };
@@ -28,7 +18,7 @@ const PostContent: React.FC<{ post: IPost }> = (props) => {
     <article>
       <PostHeader title={props.post.title} image={imagePath} />
       <ReactMarkdown
-        components={renderers}
+      components={renderers}
         children={props.post.content}
         className="text-2xl px-4 my-4 list-disc space-y-4"
       />
